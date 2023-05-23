d05221b29ad885a45c991ee5e6bb7d68875d90af
willdera <godswillezeoke@gmail.com>
2023-05-23 03:40:37 +0100

    trait superpower

--- src/lev_distance/mod.rs
+++ src/lev_distance/mod.rs
@@ -1,19 +1,40 @@
 use std::cmp::min;
+use std::fmt::Debug;
 
-pub fn entry(source: Vec<String>, dest: Vec<String>) -> Result<u8, String> {
-    if source.is_empty() || dest.is_empty() {
-        return Err(String::from("Incomplete params"));
-    };
+#[derive(Debug)]
+struct Strdiff;
 
-    let result = if source.len() == 1 && dest.len() == 1 {
-        Ok(levdist(source[0].clone(), dest[0].clone()))
-    } else if (source.len() > 1 && dest.len() > 1) && (source.len() == dest.len()) {
-        todo!()
-    } else {
-        Err(String::from("Param length mismatch"))
-    };
+trait Lv<Args> {
+    type Output;
+    fn entry(&self, args: Args) -> Self::Output;
+}
 
-    result
+impl Lv<(&str, &str)> for Strdiff {
+    type Output = u8;
+
+    fn entry(&self, args: (&str, &str)) -> Self::Output {
+        levdist(args.0.to_string(), args.1.to_string())
+    }
+}
+
+impl Lv<(Vec<&str>, Vec<&str>)> for Strdiff {
+    type Output = Result<Vec<u8>, String>;
+
+    fn entry(&self, args: (Vec<&str>, Vec<&str>)) -> Self::Output {
+        if args.0.is_empty() || args.1.is_empty() {
+            return Err("Incomplete params".to_string());
+        };
+
+        if args.0.len() == 1 && args.1.len() == 1 {
+            let result = levdist(args.0[0].to_string(), args.1[0].to_string());
+
+            return Ok(vec![result]);
+        } else if (args.0.len() > 1 && args.1.len() > 1) && (args.0.len() == args.1.len()) {
+            todo!()
+        } else {
+            return Err(String::from("Param length mismatch"));
+        };
+    }
 }
 
 fn levdist(source: String, dest: String) -> u8 {
@@ -60,4 +81,17 @@ fn test_entry() {
 
     assert!(result.is_err());
     assert_eq!(result.unwrap_err(), "Param length mismatch");
+}
+
+#[test]
+fn test_strdiff() {
+    let test = Strdiff;
+    let args = ("hello", "hella");
+    let result = test.entry(args);
+
+    let vargs = (vec!["hello"], vec!["hal"]);
+    let vresult = test.entry(vargs);
+
+    assert_eq!(result, 1);
+    assert_eq!(vresult.unwrap(), vec![3]);
 }
