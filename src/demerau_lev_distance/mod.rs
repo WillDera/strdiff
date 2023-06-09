27dc0cf1c81eb45b981c193b47ca4462ec0b56ce
willdera <godswillezeoke@gmail.com>
2023-06-09 22:46:01 +0100

    correct implementation (I guess 🤷)

--- src/demerau_lev_distance/mod.rs
+++ src/demerau_lev_distance/mod.rs
@@ -25,62 +25,62 @@ impl Dlv<(Vec<String>, Vec<String>)> for DemerauStrdiff {
 }
 
 fn demeraudist(source: String, dest: String) -> u8 {
-    let a_bytes = source.as_bytes();
-    let b_bytes = dest.as_bytes();
-    let sigma: HashSet<u8> = a_bytes.iter().chain(b_bytes.iter()).copied().collect();
-    let mut da: HashMap<u8, usize> = sigma.into_iter().map(|c| (c, 0)).collect();
-    let max_dist = source.len() + dest.len();
-    let mut d: Vec<Vec<usize>> = vec![vec![max_dist; dest.len() + 2]; source.len() + 2];
-    let mut memo: HashMap<(usize, usize), usize> = HashMap::new();
+    let source_len = source.len();
+    let dest_len = dest.len();
+    let mut mem: HashMap<(i32, i32), usize> = HashMap::new();
 
+    for i in -1..=(source_len as i32) {
+        mem.insert((i, -1), (i + 1) as usize);
+    }
+
+    for j in -1..=(dest_len as i32) {
+        mem.insert((-1, j), (j + 1) as usize);
+    }
     // helper(a_bytes, b_bytes, &mut da, &mut d, source.len(), dest.len()) as u8
     helper(
-        a_bytes,
-        b_bytes,
-        &mut da,
-        &mut d,
-        source.len(),
-        dest.len(),
-        &mut memo,
+        (source_len - 1) as i32,
+        (dest_len - 1) as i32,
+        source.as_str(),
+        dest.as_str(),
+        &mut mem,
     ) as u8
 }
 
-fn helper(
-    a: &[u8],
-    b: &[u8],
-    da: &mut HashMap<u8, usize>,
-    d: &mut Vec<Vec<usize>>,
-    i: usize,
-    j: usize,
-    memo: &mut HashMap<(usize, usize), usize>,
-) -> usize {
-    if let Some(&result) = memo.get(&(i, j)) {
-        return result;
-    }
-
-    let result = if i == 0 && j == 0 {
-        a.len() + b.len()
-    } else if i == 0 {
-        j
-    } else if j == 0 {
-        i
-    } else {
-        let cost = if a[i - 1] == b[j - 1] { 0 } else { 1 };
-        let substitution = helper(a, b, da, d, i - 1, j - 1, memo) + cost;
-        let insertion = helper(a, b, da, d, i, j - 1, memo) + 1;
-        let deletion = helper(a, b, da, d, i - 1, j, memo) + 1;
-
-        let transposition = if i > 1 && j > 1 && a[i - 1] == b[j - 2] && a[i - 2] == b[j - 1] {
-            helper(a, b, da, d, i - 2, j - 2, memo) + 1
-        } else {
-            usize::MAX
-        };
-
-        substitution.min(insertion).min(deletion).min(transposition)
+fn helper(i: i32, j: i32, s1: &str, s2: &str, d: &mut HashMap<(i32, i32), usize>) -> usize {
+    if let Some(&v) = d.get(&(i, j)) {
+        return v;
     };
 
-    memo.insert((i, j), result);
-    result
+    let (deletion, insertion, substitution) = (
+        helper(i - 1, j, s1, s2, d) + 1,
+        helper(i, j - 1, s1, s2, d) + 1,
+        helper(i - 1, j - 1, s1, s2, d)
+            + if s1.chars().nth(i as usize) == s2.chars().nth(j as usize) {
+                0
+            } else {
+                1
+            },
+    );
+    let distance = std::cmp::min(std::cmp::min(deletion, insertion), substitution);
+
+    if i > 0
+        && j > 0
+        && s1.chars().nth(i as usize) == s2.chars().nth((j - 1) as usize)
+        && s1.chars().nth((i - 1) as usize) == s2.chars().nth(j as usize)
+    {
+        let transposition = helper(i - 2, j - 2, s1, s2, d)
+            + if s1.chars().nth(i as usize) == s2.chars().nth((j - 1) as usize)
+                && s1.chars().nth((i - 1) as usize) == s2.chars().nth(j as usize)
+            {
+                1
+            } else {
+                2
+            };
+        d.insert((i, j), std::cmp::min(distance, transposition));
+    } else {
+        d.insert((i, j), distance);
+    }
+    d.get(&(i, j)).unwrap().clone()
 }
 
 #[test]
@@ -90,6 +90,6 @@ fn test_demeraudist() {
     let third = demeraudist("Abdul Hasan".to_string(), "Abdil Husain".to_string());
 
     assert_eq!(initial, 3);
-    assert_eq!(second, 4);
-    assert_eq!(third, 5);
+    assert_eq!(second, 2);
+    assert_eq!(third, 3);
 }
